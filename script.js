509af9025b8fd3d8ce1e113f10dcd4023ef18d02
brafios <brafios8@gmail.com>
2025-04-30 10:52:47 -0300

    usando sort e filter

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // filter()
 // retorna elementos que passam numa condição
 const numeros = [1, 2, 3, 4, 5];
-const pares = numeros.filter(n => n % 2 === 0);
+const pares = numeros.filter(n => n % 2 === 1);
 console.log(pares); // [2, 4]
 
 // find()
@@ -26,3 +26,29 @@ frutas.forEach(fruta => {
 const valores = [10, 20, 30];
 const total = valores.reduce((acumulador, valorAtual) => acumulador + valorAtual, 0);
 console.log(total); // 60
+
+const usuarios1 = [
+    { nome: "Lucas", idade: 17 },
+    { nome: "Julia", idade: 22 },
+    { nome: "Carlos", idade: 19 },
+    { nome: "Ana", idade: 18 },
+    { nome: "João", idade: 25 },
+    { nome: "Leo", idade: 30 }
+];
+
+const listaOrd = document.querySelector("ol")
+const maior18 = usuarios1
+    .filter(m => m.idade >= 18)
+    .sort((a,b) => a.nome.localeCompare(b.nome))
+
+maior18.forEach( usuario => {
+    const li = document.createElement("li")
+    li.textContent = `nome: ${usuario.nome} e idade: ${usuario.idade}`
+    listaOrd.appendChild(li)
+})
+
+
+
+
+
+
